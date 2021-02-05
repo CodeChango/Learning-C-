911c6b0d121473a002777a4d533f8fee137516fa
CodeChango <47929000+CodeChango@users.noreply.github.com>
2021-02-05 14:25:06 -0600

    Add files via upload

---
+++ Function with parameters/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// parameter: additional info for function to work
+using namespace std;
+
+void printText(int x){
+    cout << "My lucky number is " << x <<  endl;
+}
+
+int main()
+{
+    printText(4);
+    return 0;
+}
