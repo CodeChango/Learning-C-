7971a590b09052621b9aa40a8dc59326078ca66f
CodeChango <47929000+CodeChango@users.noreply.github.com>
2021-02-05 14:18:34 -0600

    Add files via upload

---
+++ Function/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void printText();
+
+int main()
+{
+    printText();
+    return 0;
+}
+
+void printText (){
+    cout << "Gone, reduced to atoms" << endl;
+}
+
+/* Create function before main OR
+Prototype the function */
+
